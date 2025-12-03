eb757a52c07f905fd78e39a515d578a163911daa
Noémi Salaün <noemi.salaun@gmail.com>
2025-12-03 21:38:29 +0100

    day 2 part 1

---
+++ day02/main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Range struct {
+	start int
+	end   int
+}
+
+type Input struct {
+	ranges []Range
+}
+
+func main() {
+	input := loadInput("day02/input.txt")
+	part1(input)
+	//part2(input)
+}
+
+func loadInput(filepath string) Input {
+	file, err := os.Open(filepath)
+	if err != nil {
+		log.Fatalf("Failed to open file: %s", err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var input Input
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		ranges := strings.Split(line, ",")
+		for _, tuple := range ranges {
+			parts := strings.Split(tuple, "-")
+			if len(parts) != 2 {
+				log.Fatalf("Invalid range: %s", tuple)
+			}
+			start, err := strconv.Atoi(parts[0])
+			if err != nil {
+				log.Fatalf("Invalid range part: %s", parts[0])
+			}
+			end, err := strconv.Atoi(parts[1])
+			if err != nil {
+				log.Fatalf("Invalid range part: %s", parts[1])
+			}
+			input.ranges = append(input.ranges, Range{start: start, end: end})
+		}
+	}
+
+	return input
+}
+
+func part1(input Input) {
+	var result int
+
+	for _, r := range input.ranges {
+		for id := r.start; id <= r.end; id++ {
+			if isInvalidIdPart1(id) {
+				result += id
+			}
+		}
+	}
+
+	fmt.Printf("Part 1 = %d\n", result)
+}
+
+func isInvalidIdPart1(id int) bool {
+	s := strconv.Itoa(id)
+	l := len(s)
+	if l%2 != 0 {
+		return false
+	}
+	return s[:l/2] == s[l/2:]
+}
+
+func part2(input Input) {
+	var result int
+
+	for _, r := range input.ranges {
+		for id := r.start; id <= r.end; id++ {
+			if isInvalidIdPart2(id) {
+				result += id
+			}
+		}
+	}
+
+	fmt.Printf("Part 2 = %d\n", result)
+}
+
+func isInvalidIdPart2(id int) bool {
+	// TODO part 2
+	return false
+}
