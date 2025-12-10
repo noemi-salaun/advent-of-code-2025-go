223c36ff6491f095d518116329d79be6abb93726
Noémi Salaün <noemi.salaun@gmail.com>
2025-12-10 23:09:16 +0100

    day 9 part 2

--- day09/main.go
+++ day09/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"image"
+	"image/color"
+	"image/png"
 	"log"
 	"math"
 	"os"
@@ -18,9 +21,12 @@ type Coord struct {
 type Input []Coord
 
 func main() {
-	input := loadInput("day09/input.txt")
-	part1(input)
+	//input := loadInput("day09/input.txt")
+	//part1(input)
 	//part2(input)
+
+	// Surface trouvée à la main dans Gimp à partir du PNG dessiné
+	fmt.Printf("Part 2 = %d\n", calcArea(Coord{94872, 50262}, Coord{5104, 67629}))
 }
 
 func loadInput(filepath string) Input {
@@ -68,9 +74,57 @@ func part1(input Input) {
 }
 
 func part2(input Input) {
-	var result int
+	img := image.NewRGBA(image.Rect(0, 0, 1000, 1000))
 
-	fmt.Printf("Part 2 = %d\n", result)
+	green := color.RGBA{G: 255, A: 255}
+	red := color.RGBA{R: 255, A: 255}
+
+	lastPoint := input[len(input)-1]
+	for _, nextPoint := range input {
+
+		if lastPoint.x == nextPoint.x {
+			if lastPoint.y < nextPoint.y {
+				for y := lastPoint.y; y <= nextPoint.y; y++ {
+					img.Set(lastPoint.x/100, y/100, green)
+				}
+			} else {
+				for y := nextPoint.y; y <= lastPoint.y; y++ {
+					img.Set(lastPoint.x/100, y/100, green)
+				}
+			}
+		} else if lastPoint.y == nextPoint.y {
+			if lastPoint.x < nextPoint.x {
+				for x := lastPoint.x; x <= nextPoint.x; x++ {
+					img.Set(x/100, lastPoint.y/100, green)
+				}
+			} else {
+				for x := nextPoint.x; x <= lastPoint.x; x++ {
+					img.Set(x/100, lastPoint.y/100, green)
+				}
+			}
+		} else {
+			panic("not aligned points")
+		}
+
+		lastPoint = nextPoint
+	}
+
+	for _, point := range input {
+		img.Set(point.x/100, point.y/100, red)
+	}
+
+	// Créer un fichier PNG
+	outputFile, err := os.Create("day09/output.png")
+	if err != nil {
+		panic(err)
+	}
+	defer outputFile.Close()
+
+	// Encoder l'image en PNG et l'écrire dans le fichier
+	err = png.Encode(outputFile, img)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func calcArea(a Coord, b Coord) int {
