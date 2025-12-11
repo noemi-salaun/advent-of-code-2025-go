0de34f9d6670dde83f78cbbd3736edb10a90d1c6
Noémi Salaün <noemi.salaun@gmail.com>
2025-12-11 22:01:51 +0100

    day 9 part 2 (true solution)

--- day09/main.go
+++ day09/main.go
@@ -3,30 +3,23 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"image"
-	"image/color"
-	"image/png"
 	"log"
-	"math"
 	"os"
 	"strconv"
 	"strings"
 )
 
-type Coord struct {
+type Point struct {
 	x int
 	y int
 }
 
-type Input []Coord
+type Input []Point
 
 func main() {
-	//input := loadInput("day09/input.txt")
+	input := loadInput("day09/input.txt")
 	//part1(input)
-	//part2(input)
-
-	// Surface trouvée à la main dans Gimp à partir du PNG dessiné
-	fmt.Printf("Part 2 = %d\n", calcArea(Coord{94872, 50262}, Coord{5104, 67629}))
+	part2(input)
 }
 
 func loadInput(filepath string) Input {
@@ -52,7 +45,7 @@ func loadInput(filepath string) Input {
 		if err != nil {
 			log.Fatalf("Cannot convert value %s to int", parts[1])
 		}
-		input = append(input, Coord{x, y})
+		input = append(input, Point{x, y})
 	}
 
 	return input
@@ -63,7 +56,7 @@ func part1(input Input) {
 
 	for i, a := range input {
 		for _, b := range input[i+1:] {
-			area := calcArea(a, b)
+			area := newRect(a, b).getArea()
 			if area > result {
 				result = area
 			}
@@ -74,63 +67,71 @@ func part1(input Input) {
 }
 
 func part2(input Input) {
-	img := image.NewRGBA(image.Rect(0, 0, 1000, 1000))
+	var result int
 
-	green := color.RGBA{G: 255, A: 255}
-	red := color.RGBA{R: 255, A: 255}
-
-	lastPoint := input[len(input)-1]
-	for _, nextPoint := range input {
-
-		if lastPoint.x == nextPoint.x {
-			if lastPoint.y < nextPoint.y {
-				for y := lastPoint.y; y <= nextPoint.y; y++ {
-					img.Set(lastPoint.x/100, y/100, green)
-				}
-			} else {
-				for y := nextPoint.y; y <= lastPoint.y; y++ {
-					img.Set(lastPoint.x/100, y/100, green)
-				}
+	for i, a := range input {
+		for _, b := range input[i+1:] {
+			rect := newRect(a, b)
+			area := rect.getArea()
+			if area > result && rect.isValid(&input) {
+				result = area
 			}
-		} else if lastPoint.y == nextPoint.y {
-			if lastPoint.x < nextPoint.x {
-				for x := lastPoint.x; x <= nextPoint.x; x++ {
-					img.Set(x/100, lastPoint.y/100, green)
-				}
-			} else {
-				for x := nextPoint.x; x <= lastPoint.x; x++ {
-					img.Set(x/100, lastPoint.y/100, green)
-				}
-			}
-		} else {
-			panic("not aligned points")
+		}
+	}
+
+	fmt.Printf("Part 2 = %d\n", result)
+}
+
+type Rect struct {
+	top    int
+	right  int
+	bottom int
+	left   int
+}
+
+func newRect(a Point, b Point) *Rect {
+	var rect Rect
+
+	if a.x < b.x {
+		rect.left = a.x
+		rect.right = b.x
+	} else {
+		rect.left = b.x
+		rect.right = a.x
+	}
+
+	if a.y < b.y {
+		rect.bottom = a.y
+		rect.top = b.y
+	} else {
+		rect.bottom = b.y
+		rect.top = a.y
+	}
+
+	return &rect
+}
+
+func (rect *Rect) getArea() int {
+	return ((rect.right - rect.left) + 1) * ((rect.top - rect.bottom) + 1)
+}
+
+func (rect *Rect) intersectWith(otherRect *Rect) bool {
+	return rect.left < otherRect.right && rect.right > otherRect.left && rect.top > otherRect.bottom && rect.bottom < otherRect.top
+}
+
+func (rect *Rect) isValid(input *Input) bool {
+
+	lastPoint := (*input)[len(*input)-1]
+	for _, nextPoint := range *input {
+
+		otherRect := newRect(lastPoint, nextPoint)
+
+		if rect.intersectWith(otherRect) {
+			return false
 		}
 
 		lastPoint = nextPoint
 	}
 
-	for _, point := range input {
-		img.Set(point.x/100, point.y/100, red)
-	}
-
-	// Créer un fichier PNG
-	outputFile, err := os.Create("day09/output.png")
-	if err != nil {
-		panic(err)
-	}
-	defer outputFile.Close()
-
-	// Encoder l'image en PNG et l'écrire dans le fichier
-	err = png.Encode(outputFile, img)
-	if err != nil {
-		panic(err)
-	}
-}
-
-func calcArea(a Coord, b Coord) int {
-	return (absInt(a.x-b.x) + 1) * (absInt(a.y-b.y) + 1)
-}
-
-func absInt(n int) int {
-	return int(math.Abs(float64(n)))
+	return true
 }
